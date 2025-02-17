a4eb3879a4853848361c40377c5ba3625b84f40d
Gilles <quodbach.gilles@gmail.com>
2025-02-17 14:00:53 +0100

    ajout de ModelAdmin + tri et filtre (4)

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date"]
+    list_filter = ["question_text", "pub_date"]
+    ordering = ["pub_date"]
+    search_fields = ["question_text", "pub_date"]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["choice_text", "votes", "question_id"]
+    list_filter = ["choice_text", "votes", "question_id"]
+    ordering = ["question_id"]
+    search_fields = ["choice_text"]
+
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+
+
+
+
