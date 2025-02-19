165002b98fe861b99e3148ffa13d08075d1107e4
Gilles <quodbach.gilles@gmail.com>
2025-02-19 11:44:28 +0100

    partie 7 modif admin ok

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ["question_text", "pub_date"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
     list_filter = ["question_text", "pub_date"]
     ordering = ["pub_date"]
     search_fields = ["question_text", "pub_date"]
     # en changeant l'ordre on modifie l'affichage dans l'admin
-    fields = ["pub_date","question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
 
 
 class ChoiceAdmin(admin.ModelAdmin):
